19a03222dd8ad6014e7ddffcfa3a259dee09b158
bbstrees <bbstrees@hotmail.com>
2017-12-06 10:27:24 +0800

    123

--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -6,23 +6,26 @@ package singleton;
 
 public class Singleton {  
 	  
-    /* 持有私有静态实例，防止被引用，此处赋值为null，目的是实现延迟加载 */  
-    private static Singleton instance = null;  
-  
     /* 私有构造方法，防止被实例化 */  
     private Singleton() {  
     }  
   
-    /* 静态工程方法，创建实例 */  
+    /* 此处使用一个内部类来维护单例
+     * 
+     * 静态内部类在外部类被构造时候加载，JVM内部的机制能够保证当一个类被加载的时候，这个类的加载过程是线程互斥的。
+     * 这样当我们第一次调用getInstance的时候，JVM能够帮我们保证instance只被创建一次
+     *  */  
+    private static class SingletonFactory {  
+        private static Singleton instance = new Singleton();  
+    }  
+  
+    /* 获取实例 */  
     public static Singleton getInstance() {  
-        if (instance == null) {  
-            instance = new Singleton();  
-        }  
-        return instance;  
+        return SingletonFactory.instance;  
     }  
   
     /* 如果该对象被用于序列化，可以保证对象在序列化前后保持一致 */  
     public Object readResolve() {  
-        return instance;  
+        return getInstance();  
     }  
 }  
